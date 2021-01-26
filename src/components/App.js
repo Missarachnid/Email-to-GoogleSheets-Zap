f91357e84e817a48079b2a29c8b6e6357a4d2440
Melissa Kepler <mmkepler@yahoo.com>
2021-01-26 15:25:05 -0700

    feat: form structure added

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import '../css/App.css';
 import { useSelector } from 'react-redux';
+import { StyledForm } from './styles/styledForm';
 import {
   FIRST_NAME_INPUT,
   LAST_NAME_INPUT,
@@ -12,11 +13,76 @@ import {
 function App() {
   const formData = useSelector(state => state.formData)
   return (
-    <div className="App">
-      <form>
-        Form goes here
-      </form>
-    </div>
+    <StyledForm>
+      <h1>Form to Google Doc Zap</h1>
+      <h2>Press Submit and Your information will be sent to me<br />and added to a google doc!</h2>
+      <div className='form-group'>
+        <form id='form'>
+          <label>First Name
+            <br />
+            <input 
+            type='text'
+            id='firstNameInput'
+            placeholder='Fist Name'
+            required
+            />
+          </label>
+          <br />
+          <label>Last Name
+            <br />
+            <input 
+            type='text'
+            id='lastNameInput'
+            placeholder='Last Name'
+            required
+            />
+          </label>
+          <br />
+          <label>User Name
+            <br />
+            <input 
+            type='text'
+            id='userNameInput'
+            placeholder='User Name'
+            required
+            />
+          </label>
+          <br />
+          <label>Email
+          <br />
+          <input type="email" 
+            className="form-control email-form" 
+            id="emailInput" placeholder="Email" 
+            name="emailInput" 
+            aria-required="true"
+            required/>
+          </label>
+          <br />
+          <label>Message
+          <br />
+          <textarea 
+            form="form" 
+            className="form-control email-form" 
+            id="messageInput" 
+            name="messageInput" 
+            placeholder="What's up?" 
+            cols="20" 
+            rows="5" 
+            aria-required="true"
+            required>
+          </textarea>
+          </label>
+          <br />
+          <button 
+            type="submit" 
+            id="submit-button"
+            className="btn btn-default" 
+            >
+              Submit
+            </button>
+        </form>
+      </div>
+    </StyledForm>
   );
 }
 
