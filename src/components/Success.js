dfb5163ff2f5abf550578d92cc9117badfd952e2
Melissa Kepler <mmkepler@yahoo.com>
2021-02-05 17:10:27 -0700

    fix: css issue

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { StyledResults } from './styles/StyledResults'
 
 const Success = () => {
   return(
-    <div>
+    <StyledResults>
       <h1>Success! Your information has been sent.</h1>
       <a href='https://docs.google.com/spreadsheets/d/1n8XswAXn9Hr7nz1qfhXRRQCo7iavQkaYO7Aur9TNDdc/edit?usp=sharing' rel='noreferrer noopener' target='_blank'>The sheet can be viewed here</a>
-    </div>
+    </StyledResults>
   )
   } 
 export default Success;
